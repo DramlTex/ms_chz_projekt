81cae73a2b3d479e10e9ecae8de73b5a969d647c
Mark <holy_dragon@mail.ru>
2025-09-20 18:15:34 +0300

    delited all

--- prototypes/order-app/utils/wait.js
+++
@@ -1,7 +0,0 @@
-/**
- * Возвращает промис, который резолвится через указанное время.
- * @param {number} ms
- */
-export function wait(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
